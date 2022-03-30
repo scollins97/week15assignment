4e81d8c145f77868efcce0b5752ddc37a8bc03a2
scollins97 <srcollins575@gmail.com>
2022-03-30 18:49:13 -0400

    week 16 submission

--- src/main/java/com/promineotech/jeep/entity/Jeep.java
+++ src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -1,17 +1,18 @@
 package com.promineotech.jeep.entity;
 
 import java.math.BigDecimal;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Comparator;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Jeep {
+public class Jeep implements Comparable<Jeep> {
 	
 	private Long modelPK;
 	private JeepModel modelId;
@@ -19,5 +20,20 @@ public class Jeep {
 	private int numDoors;
 	private int wheelSize;
 	private BigDecimal basePrice;
+	
+	@JsonIgnore
+	public Long getModelPK() {
+		return modelPK;
+	}
 
+	@Override
+	public int compareTo(Jeep that) {
+		// @formatter:off
+		return Comparator
+				.comparing(Jeep::getModelId)
+				.thenComparing(Jeep::getTrimLevel)
+				.thenComparing(Jeep::getNumDoors)
+				.compare(this, that);
+		//@formatter:on
+	}
 }
